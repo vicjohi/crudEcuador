9a5ce5bc63b9e858d9a84f78fb89186ae8fc84fa
vicjohi <vicjohi@gmail.com>
2022-09-30 18:03:41 -0500

    Se agrega proyecto

---
+++ src/main/java/com/nttdata/crudEcuador/CrudEcuadorApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.crudEcuador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEcuadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEcuadorApplication.class, args);
+	}
+
+}
